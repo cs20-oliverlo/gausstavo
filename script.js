25f4c248b9ddbbf7ddfc5d28f622e5cdc40147e0
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-12 11:08:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Script.js
+
+// HTML Elements
+let calcBtn = document.getElementById("calc-btn");
+let sumOut = document.getElementById("sum-out");
+
+// Add Event Listener
+calcBtn.addEventListener("click", calcSum);
+
+// Event Function
+function calcSum() {
+    // Print integers 1 to 100
+    let total = 0;
+    let input = document.getElementById("n-in").value;
+    
+    for (let n = 1; n <= input; n++) {
+        total = total + n;
+    }
+
+    // Output the Total
+    sumOut.innerHTML = total;
+}
